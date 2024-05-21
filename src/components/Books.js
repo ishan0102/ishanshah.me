fa9c19ecbeefeabcd696d7923044bc62d65848b0
marmik <103449011+MarmikChaudhari@users.noreply.github.com>
2024-05-21 11:50:43 +0530

    updated books & interests

--- src/components/Books.js
+++ src/components/Books.js
@@ -15,14 +15,38 @@ export function Books() {
     <div className="relative flex max-h-screen w-full flex-col overflow-y-scroll scrollbar-hide scroll-smooth px-8">
       <div className="mx-auto w-full max-w-2xl mt-4 sm:mt-8 mb-52 md:mb-32">
         <div className="prose mb-2 sm:mb-6" >
-          i love reading & my fav genre of books is sci-fi and biographies.
+
+        <p style={{fontSize:'16px'}}> "a shelf of books is a shelf of many lives and ideas and imaginations.
+        the true lover of books enjoys all kinds of books, even some nonsense now and then, because enjoying nonsense from others can teach us to also laugh at ourselves. 
+        a person who does not learn to laugh at his own problems and weaknesses and foolishness can never be a truly educated or a truly happy person. also, probably the same thing could be said of a person who does not enjoy learning and growing all his life."</p>
+
+         
+          i love reading almost all types of stuff but my fav genre is sci-fi and biographies.
           list of books that i've read in the past :
+
+          <p style={{fontSize:'20px',fontWeight:'bold', color:'black'}}></p>
+
 
           <p style={{fontSize:'20px',fontWeight:'bold', color:'black'}}>2024</p>
 
           <ul>
 
-            <li>children of dune</li>
+            <li>children of dune (currently reading) </li>
+
+            <li>surely you're joking, mr. feynman</li>
+
+            <li>looking for alaska</li>
+
+            <li>the great gatsby</li>
+
+            <li>dune messiah</li>
+
+            <li>dune</li>
+
+            <li>foundation</li>
+
+            <li>the fellowship of the ring</li>
+
 
           </ul>
 
@@ -33,14 +57,38 @@ export function Books() {
             
             <li>1984</li>
 
+            <li>the lightning thief</li>
+
+            <li>and then there were none</li>
+
+            <li>the fault in our stars</li>
+
+
             
           </ul>
 
           <p style={{fontSize:'20px',fontWeight:'bold', color:'black'}}>2022 & beyond</p>
 
           <ul>
-            
-            <li>steve jobs</li>
+
+            <li>sapiens</li>
+
+            <li>steve jobs (all time fav.)</li>
+
+            <li>man's search for meaning</li>
+
+            <li>21 lessons for the 21st century</li>
+
+            <li>how google works</li>
+
+            <li>the innovators</li>
+
+            <li>einstein : his life and universe</li>
+
+            <li>atomic habits</li>
+
+            <li>deep work</li>
+
             
           </ul>
 
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,12 +3,22 @@ export function Interests() {
       <div className="relative flex max-h-screen w-full flex-col overflow-y-scroll scrollbar-hide scroll-smooth px-8">
         <div className="mx-auto w-full max-w-2xl mt-4 sm:mt-8 mb-52 md:mb-32">
           <div className="prose mb-2 sm:mb-6" >
-            things that i want to explore or learn about :           
+            things that i want to explore/build or learn about :           
   
             <ul>
   
-              <li>gpu programming</li>
-  
+              <li>GPUs & CUDA programming</li>
+
+              <li>alphafold & the protein folding problem</li>
+
+              <li>computational neuroscience/cog sci</li>
+
+              <li>lisp</li>
+
+              <li>my neural network library</li>
+
+              <li>fourier transforms</li>
+
            
               
             </ul>
